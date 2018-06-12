b924fb17dfccbd0339b4167c503345da7f4e4819
svsramareddy <40166780+svsramareddy@users.noreply.github.com>
2018-06-12 07:48:48 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,85 @@
+﻿import socket, threading
+
+""" Function to send messages to particular person or to particular group(by group id) and data will be in the format (string)FROM ~ GROUP_ID ~ TO ~ MESSAGE """
+
+pub_id = ''
+def send():
+    while True:
+        
+        response = input("If you are a new user enter 'Y' else enter 'N' ")
+
+        if response == 'N':
+            pr_uid = input("Enter the private ID provided ")
+            server.send(pr_uid)
+            break
+        else if response == 'Y':
+            server.send(response)
+            ids = server.recv(4096)
+            ids = ids.split("~")    # Returns a list ['private_id', 'public_id']
+            priv_id = ids[0]
+            global pub_id
+            pub_id = ids[1]
+            print("Remember this private unique id {0} for further login \nShare this public id {} with people to message you".format(priv_id,pub_id))
+            break
+        else:
+            print("Invalid input")
+
+
+    while True:
+
+        # Public id starts with 'p' and group id starts with 'g'
+        to = input("Enter the id of recipient/group id (you must be a member of the group)")
+        to = to.strip()
+        print("Enter 'Q' to close the chat with {0}".format(to))
+
+        tobe_added = ''
+        
+        if to[0] == 'g':    # Message is to group
+            tobe_added = "~".join([pub_id,to,''])     # data format 'FROM ~ GROUP_ID ~ TO ~ MESSAGE'
+        else:               # Means meassage is to person
+            tobe_added = "~".join([pub_id,'',to])
+
+        while True:
+            
+            msg = input('\nMe > ')
+            if msg == 'Q':
+                break
+            data = "~".join([tobe_added,msg])
+
+            server.send(data)
+
+
+""" For receiving messages from other people. Received 'data' contains information about SENDER, GROUP_DETAILS, RECEIVER and MESSAGE """
+
+def receive():
+    while True:
+        
+        data = server.recv(4096)
+        data = data.strip()
+        data = data.split("~")
+        
+        """ Now data is a list of strings as follows ['FROM','GROUP_ID','TO','MESSAGE']
+        No two groups can have same GROUP_ID """
+
+        if data[1] == '':   # Checking if message is not to group
+            print{"FROM: {0}: {1}".format(data[0],data[3]))
+
+        else:   # message should be displayed in particular GROUP 
+            print{"TO GROUP {0}: {1}".format(data[1],data[3]))
+
+
+if __name__ == "__main__":   
+    # TCP socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    HOST = 'localhost'
+    PORT = 8080
+    #Connecting to server
+    server.connect((HOST, PORT))     
+    print('Connected to remote host...')
+    
+    thread_send = threading.Thread(target = send)
+    thread_send.start()
+
+    thread_receive = threading.Thread(target = receive)
+    thread_receive.start()
---
+++ server.py
@@ -0,0 +1,90 @@
+﻿import socket, threading
+
+""" Server accepts connections from clients and checks whether they are new users or existing users
+For new users it sends private_id (useful for next time logins) and public_id( to share with others to send message to user)
+If they are old users then server checks private private_id and replaces the last time socket object with latest one for communication with client
+
+For every message it receives it checks whether it should be routed to GROUP or to INDIVIDUAL and sends accordingly"""
+
+# groups is dictionary maintaining lists of groups and public keys of group members
+groups = {'g1':['pub1','pub3','pub5'],'g2':['pub2','pub4','pub6']}
+
+# connection_list is dictionary maintaining list of public id as keys and their corresponding socket object(lastest) as values 
+connection_list = {}
+
+""" ids is dictionary maintaining private id as keys and public id as values
+This is userful for login and establishing connection to client """
+
+ids = {'priv1':'pub1','priv2':'pub2','priv3':'pub3','priv4':'pub4','priv5':'pub5','priv6':'pub6'}   # List of ids of sign up users
+
+
+
+# Function accepts clients and verifies whether they are new users or existing users
+
+def accept_client():
+    while True:
+        #accept    
+        cli_sock, cli_add = ser_sock.accept()
+        response = cli_sock.recv(4096)
+        response = response.strip()
+        
+        # Checking whether new user or not
+        if response == 'Y':
+            num = str(len(list(ids.keys()))+1)
+            pri_id = 'priv' + num
+            pub_id = 'pub' + num
+            ids[pri_id] = pub_id
+            connection_list[pub_id] = cli_sock
+        
+            data = "~".join([pri_id,pub_id])
+            cli_sock.send(data)
+
+        else:
+            pri_id = cli_sock.recv(4096)
+
+            for key in ids.keys():
+                if key == pri_id:
+                    pub_id = ids[pri_id]
+                    connection_list[pub_id] = cli_sock  # Replacing old socket object with new one
+                    break
+                    
+
+        thread_client = threading.Thread(target = handle_usr, args=(cli_sock,))
+        thread_client.start()
+
+def handle_usr(cli_sock):
+    while True:
+        try:
+            data = cli_sock.recv(4096)
+            if data:        # data format 'FROM ~ GROUP_ID ~ TO ~ MESSAGE'
+                check = data.split("~")
+                
+                if check[1] == '':   # Message is not to group
+                    send_to_user(connection_list[check[2]],data)
+                    
+                else:   # Message should be sent to all members of group
+                    for member in groups[check[1]]:
+                        send_to_user(connection_list[member],data)
+                    
+        except:
+            continue
+
+#  Sending messages to user_sock
+def send_to_user(user_sock, msg):
+    user_sock.send(msg)
+
+if __name__ == "__main__":    
+
+    # socket
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # bind
+    HOST = 'localhost'
+    PORT = 8080
+    server_sock.bind((HOST, PORT))
+
+    # listen    
+    server_sock.listen(5)
+
+    thread_accept = threading.Thread(target = accept_client)
+    thread_accept.start()
